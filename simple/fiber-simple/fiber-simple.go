58c926ed52114f728b1d0e42cbafffd7504ce99b
siim <siimpuustusmaa@gmail.com>
2021-04-15 23:49:22 +0300

    made fiber app more explicit as gin is

--- simple/fiber-simple/fiber-simple.go
+++ simple/fiber-simple/fiber-simple.go
@@ -6,8 +6,9 @@ func main() {
     app := fiber.New()
 
     app.Get("/", func(c *fiber.Ctx) error {
-		c.Set("Content-Type", "text/plain")
-        return c.SendString("hello world")
+        c.Status(200)
+		c.Set("Content-Type", "text/plain; charset=utf-8")
+		return c.Send([]byte("hello world"))
     })
 
     app.Listen(":8080")
